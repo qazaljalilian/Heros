29597a90460a4e4ae64ef73237287b6094912d33
Qazal Jalilian <qazal.jalilian@palettesoftware.com>
2021-11-24 16:05:43 +0100

    future components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HerosComponent } from './components/heros/heros.component';
+import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HerosComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
